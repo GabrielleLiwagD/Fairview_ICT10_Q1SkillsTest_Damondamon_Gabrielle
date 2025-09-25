3b0ebd848e53d97b3a4cb721c5e05028a70f6cbd
Gabrielle L. Damondamon <damondamon.gl@obmontessori.edu.ph>
2025-09-25 12:30:14 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+from pyscript import document, display
+
+def information(event=None):
+    info1 = str(document.getElementById("input1").value) #Name
+    info2 = str(document.getElementById("input2").value) #Age
+    info3 = str(document.getElementById("input3").value) #School Name
+    message = "Name: " + str(info1)
+    message2 = "Contact Number: " + str(info2)
+    message3 = "Address: " + str(info3)
+    food = str(document.getElementById("food-order").value) #Food Order
+    
+    if food == "Potato-Leek Soup":
+        price = 450
+    elif food == "Creamy and Cheesy Mashed Potatoes":
+        price = 300
+    elif food == "Potato-Leek Stir-Fry":
+        price = 500
+    elif food == "Potato-Leek Salad":   
+        price = 550
+    else:
+        price = 0
+    
+    total_price = f"Total Price: PHP {price}"
+    display(total_price, target="total")
+    display(message, target="details1", append=True)
+    display(message2, target="details2", append=True)
+    display(message3, target="details3", append=True)
